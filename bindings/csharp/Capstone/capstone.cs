ed73104d8c8c5bd1e9adf8a5355cef7f9823ae55
mattifestation <mattgraeber@gmail.com>
2014-01-03 18:33:43 -0500

    Added initial C# binding to Capstone

---
+++ bindings/csharp/Capstone/capstone.cs
@@ -0,0 +1,387 @@
+﻿// Capstone C# bindings
+// By Matt Graeber <@mattifestation>, 2013>
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace Capstone
+{
+    public struct Instruction
+    {
+	    public Object Id;
+	    public ulong Address;
+	    public ushort Size;
+        public byte[] Bytes;
+	    public string Mnemonic;
+	    public string Operands;
+	    public Object[] Regs_read;
+        public Object[] Regs_write;
+        public Object[] Groups;
+        public Object Arch;
+
+        public override string ToString()
+        {
+            return  "0x" + this.Address.ToString("X8") + ": " +
+                this.Mnemonic.PadRight(8) + this.Operands;
+        }
+    }
+
+    internal class CsStatus
+    {
+        private string message;
+        private ErrorCode _err;
+        public ErrorCode Error
+        {
+            get { return this._err; }
+        }
+
+        internal CsStatus(ErrorCode capstoneError)
+        {
+            this._err = capstoneError;
+
+            switch (capstoneError)
+            {
+                case ErrorCode.Mem:
+                    this.message = "Out of memory (CsError)";
+                    break;
+                case ErrorCode.Arch:
+                    this.message = "Invalid architecture (CsError)";
+                    break;
+                case ErrorCode.Handle:
+                    this.message = "Invalid handle (CsError)";
+                    break;
+                case ErrorCode.Csh:
+                    this.message = "Invalid csh (CsError)";
+                    break;
+                case ErrorCode.Mode:
+                    this.message = "Invalid mode (CsError)";
+                    break;
+                case ErrorCode.Option:
+                    this.message = "Invalid option (CsError)";
+                    break;
+                default:
+                    this.message = "";
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.message;
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            ErrorCode err = (ErrorCode)obj;
+            if ((object)err == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return this._err == err;
+        }
+
+        public static bool operator ==(CsStatus a, ErrorCode b)
+        {
+            return a._err == b;
+        }
+
+        public static bool operator !=(CsStatus a, ErrorCode b)
+        {
+            return !(a._err == b);
+        }
+
+        public static implicit operator CsStatus(ErrorCode err)
+        {
+            return new CsStatus(err);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.message.GetHashCode();
+        }
+    }
+
+    [Serializable]
+    public class CapstoneException : Exception
+    {
+        public CapstoneException() : base("") { }
+
+        public CapstoneException(string message) : base(message) { }
+
+        internal CapstoneException(CsStatus message) : base(message.ToString()) { }
+
+        protected CapstoneException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    internal static class NativeMethods
+    {
+        #if __MonoCs__
+        [DllImport("libcapstone.so", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_open(Architecture arch, Mode mode, ref UIntPtr handle);
+        [DllImport("libcapstone.so", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern UIntPtr cs_disasm_dyn(UIntPtr handle, byte[] code,
+                UIntPtr code_size, ulong address, UIntPtr count, ref IntPtr insn);
+        [DllImport("libcapstone.so", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool cs_free(IntPtr insn);
+        [DllImport("libcapstone.so", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_close(UIntPtr handle);
+        [DllImport("libcapstone.so", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_errno(UIntPtr handle);
+        [DllImport("libcapstone.so", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void cs_version(ref int major, ref int minor);
+        [DllImport("libcapstone.so", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_option(UIntPtr handle, OptionType type, OptionValue value);
+        #else
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool SetDllDirectory(string lpPathName);
+        [DllImport("libcapstone.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_open(Architecture arch, Mode mode, ref UIntPtr handle);
+        [DllImport("libcapstone.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern UIntPtr cs_disasm_dyn(UIntPtr handle, byte[] code,
+                UIntPtr code_size, ulong address, UIntPtr count, ref IntPtr insn);
+        [DllImport("libcapstone.dll", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool cs_free(IntPtr insn);
+        [DllImport("libcapstone.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_close(UIntPtr handle);
+        [DllImport("libcapstone.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_errno(UIntPtr handle);
+        [DllImport("libcapstone.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void cs_version(ref int major, ref int minor);
+        [DllImport("libcapstone.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ErrorCode cs_option(UIntPtr handle, OptionType type, OptionValue value);
+        #endif
+    }
+
+    public class Capstone : IDisposable
+    {
+        private bool _disposed;
+        private UIntPtr _handle;
+        private CsStatus _status;
+        private Architecture _arch;
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct _Insn
+        {
+            public uint id;
+            public ulong address;
+            public ushort size;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+            public byte[] bytes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string mnemonic;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 96)]
+            public string operands;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            public int[] regs_read;
+            public uint regs_read_count;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            public int[] regs_write;
+            public uint regs_write_count;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public int[] groups;
+            public uint groups_count;
+        };
+        public Architecture Architecture
+        {
+            get { return this._arch; }
+        }
+        public Version Version
+        {
+            // This would normally be a static property but
+            // I need to make sure SetDllDirectory is called
+            // so that the correct 32 vs. 64-bit library is
+            // loaded first.
+            get
+            {
+                int major = 0;
+                int minor = 0;
+
+                NativeMethods.cs_version(ref major, ref minor);
+                return new Version(major, minor);
+            }
+        }
+
+        public Capstone(Architecture arch, Mode mode)
+        {
+            #if !____MonoCs__Cs__
+            // Get the path to the respective unmanaged library
+            string libPath = Path.Combine(Path.GetDirectoryName(typeof(Capstone._Insn).Assembly.Location),
+                (IntPtr.Size == 8 ? "x64" : "x86"));
+
+            // Set the path of the respective unmanaged library
+            NativeMethods.SetDllDirectory(libPath);
+            #endif
+
+            this._arch = arch;
+            this._status = NativeMethods.cs_open(arch, mode, ref this._handle);
+
+            if (this._status != ErrorCode.Ok)
+            {
+                throw new CapstoneException(this._status);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                NativeMethods.cs_close(this._handle);
+                this._handle = UIntPtr.Zero;
+
+                this._disposed = true;
+            }
+        }
+
+        ~Capstone()
+        {
+            Dispose(false);
+        }
+
+        public void SetDetail(bool opt)
+        {
+            if (opt == true)
+            {
+                this._status = NativeMethods.cs_option(this._handle, OptionType.Detail, OptionValue.On);
+            }
+            else
+            {
+                this._status = NativeMethods.cs_option(this._handle, OptionType.Detail, OptionValue.Off);
+            }
+
+            if (this._status != ErrorCode.Ok)
+            {
+                throw new CapstoneException(this._status);
+            }
+        }
+
+        public void SetSyntax(OptionValue opt)
+        {
+            if ((opt == OptionValue.SyntaxATT) || (opt == OptionValue.SyntaxIntel))
+            {
+                this._status = NativeMethods.cs_option(this._handle, OptionType.Syntax, opt);
+            }
+
+            if (this._status != ErrorCode.Ok)
+            {
+                throw new CapstoneException(this._status);
+            }
+        }
+
+        public Instruction[] Disassemble(byte[] code, ulong addr, UIntPtr count)
+        {
+            IntPtr ptr = IntPtr.Zero;
+
+            if (code.Length == 0) { return null; }
+
+            ulong numInst = (ulong) NativeMethods.cs_disasm_dyn(this._handle, code, (UIntPtr)(uint)code.Length, addr, count, ref ptr);
+
+            if (numInst > 0)
+            {
+                // TODO: dynamically calculate size
+                // TODO: fix alignment issues in Cs* structs
+                #if __MonoCs__
+                ulong size = 1108;
+                #else
+                ulong size = 1112;
+                #endif
+
+                Instruction[] insns = new Instruction[numInst];
+
+                for (ulong j = 0; j < numInst; j++)
+                {
+                    IntPtr data = new IntPtr(ptr.ToInt64() + (Int64)(size * j));
+
+                    // Point to architecture specific structs
+                    IntPtr archPtr = new IntPtr(data.ToInt64() + (Int64)(Marshal.SizeOf(typeof(_Insn))));
+
+                    _Insn _insn = (_Insn)Marshal.PtrToStructure(data, typeof(_Insn));
+                    insns[j].Address = _insn.address;
+                    insns[j].Size = _insn.size;
+                    insns[j].Bytes = _insn.bytes;
+                    insns[j].Mnemonic = _insn.mnemonic;
+                    insns[j].Operands = _insn.operands;
+
+                    if (_insn.regs_read_count > 0)
+                    {
+                        insns[j].Regs_read = new Object[_insn.regs_read_count];
+                        Array.Copy(_insn.regs_read, insns[j].Regs_read, _insn.regs_read_count);
+                    }
+
+                    if (_insn.regs_write_count > 0)
+                    {
+                        insns[j].Regs_write = new Object[_insn.regs_write_count];
+                        Array.Copy(_insn.regs_write, insns[j].Regs_write, _insn.regs_write_count);
+                    }
+
+                    if (_insn.groups_count > 0)
+                    {
+                        insns[j].Groups = new Object[_insn.groups_count];
+                        Array.Copy(_insn.groups, insns[j].Groups, _insn.groups_count);
+                    }
+
+                    // Cast architecture-specific enums/structs. This is ugly.
+                    // I'm open to suggesstions for improvement.
+                    switch (_arch)
+                    {
+                        case Architecture.Arm:
+                            insns[j].Id = (Arm.INSN) _insn.id;
+                            insns[j].Arch = Marshal.PtrToStructure(archPtr, typeof(Arm.CsArm));
+                            if (_insn.regs_read_count > 0) { for (int k = 0; k < insns[j].Regs_read.Length; k++) { insns[j].Regs_read[k] = (Arm.REG)insns[j].Regs_read[k]; }; }
+                            if (_insn.regs_write_count > 0) { for (int k = 0; k < insns[j].Regs_write.Length; k++) { insns[j].Regs_write[k] = (Arm.REG)insns[j].Regs_write[k]; }; }
+                            if (_insn.groups_count > 0) { for (int k = 0; k < insns[j].Groups.Length; k++) { insns[j].Groups[k] = (Arm.GRP)insns[j].Groups[k]; }; }
+                            break;
+                        case Architecture.Arm64:
+                            insns[j].Id = (Arm64.INSN) _insn.id;
+                            insns[j].Arch = Marshal.PtrToStructure(archPtr, typeof(Arm64.CsArm64));
+                            if (_insn.regs_read_count > 0) { for (int k = 0; k < insns[j].Regs_read.Length; k++) { insns[j].Regs_read[k] = (Arm64.REG)insns[j].Regs_read[k]; }; }
+                            if (_insn.regs_write_count > 0) { for (int k = 0; k < insns[j].Regs_write.Length; k++) { insns[j].Regs_write[k] = (Arm64.REG)insns[j].Regs_write[k]; }; }
+                            if (_insn.groups_count > 0) { for (int k = 0; k < insns[j].Groups.Length; k++) { insns[j].Groups[k] = (Arm64.GRP)insns[j].Groups[k]; }; }
+                            break;
+                        case Architecture.Mips:
+                            insns[j].Id = (Mips.INSN) _insn.id;
+                            insns[j].Arch = Marshal.PtrToStructure(archPtr, typeof(Mips.CsMips));
+                            if (_insn.regs_read_count > 0) { for (int k = 0; k < insns[j].Regs_read.Length; k++) { insns[j].Regs_read[k] = (Mips.REG)insns[j].Regs_read[k]; }; }
+                            if (_insn.regs_write_count > 0) { for (int k = 0; k < insns[j].Regs_write.Length; k++) { insns[j].Regs_write[k] = (Mips.REG)insns[j].Regs_write[k]; }; }
+                            if (_insn.groups_count > 0) { for (int k = 0; k < insns[j].Groups.Length; k++) { insns[j].Groups[k] = (Mips.GRP)insns[j].Groups[k]; }; }
+                                break;
+                        case Architecture.X86:
+                            insns[j].Id = (X86.INSN) _insn.id;
+                            insns[j].Arch = Marshal.PtrToStructure(archPtr, typeof(X86.CsX86));
+                            if (_insn.regs_read_count > 0) { for (int k = 0; k < insns[j].Regs_read.Length; k++) { insns[j].Regs_read[k] = (X86.REG)insns[j].Regs_read[k]; }; }
+                            if (_insn.regs_write_count > 0) { for (int k = 0; k < insns[j].Regs_write.Length; k++) { insns[j].Regs_write[k] = (X86.REG)insns[j].Regs_write[k]; }; }
+                            if (_insn.groups_count > 0) { for (int k = 0; k < insns[j].Groups.Length; k++) { insns[j].Groups[k] = (X86.GRP)insns[j].Groups[k]; }; }
+                            break;
+                    }
+                }
+
+                NativeMethods.cs_free(ptr);
+
+                return insns;
+            }
+            else
+            {
+                this._status = NativeMethods.cs_errno(this._handle);
+
+                if (this._status != ErrorCode.Ok)
+                {
+                    throw new CapstoneException(this._status);
+                }
+
+                return null;
+            }
+        }
+    }
+}
